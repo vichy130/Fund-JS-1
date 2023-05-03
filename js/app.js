7860402cd06f5d64e4a45be6040c48456ab6316c
vichy130 <viki_ccg@hotmail.com>
2023-05-02 22:59:25 -0500

    avance 2

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var cargarCabecero = ()=>{
+    var presupuesto= totalIngresos()-totalEgresos();
+    var porcentajeEgreso= totalEgresos()/totalIngresos();
+}
+var totalIngresos=()=>{
+    let totalIngreso=0;
+    let ingreso=[];
+    for(let elemento of ingreso){totalIngreso+=elemento};
+    return totalIngresos;
+}
+var totalEgresos=()=>{
+    let totalEgreso=0;
+    let egreso=[];
+    for(let elemento of egreso){totalEgreso+=elemento};
+    return totalEgreso;
+}
